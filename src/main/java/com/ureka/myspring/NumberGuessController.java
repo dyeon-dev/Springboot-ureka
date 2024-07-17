b7644f8909a9433cddf866707c88aa9eb8a7508b
kimdayeon37 <kdy37912@naver.com>
2024-07-17 21:22:16 +0900

    숫자맞추기 게임 수정

--- src/main/java/com/ureka/myspring/NumberGuessController.java
+++ src/main/java/com/ureka/myspring/NumberGuessController.java
@@ -12,12 +12,14 @@ public class NumberGuessController {
 	
 	@GetMapping("/check_num")
 	@ResponseBody
-	public String check_num(@RequestParam(value="re") int num) {
+	public String check_num(@RequestParam(value="mynum") int num) {
 		if(com_num == 0) {//난수생성전
 			com_num = (int)(Math.random()*100+1);
 		}
-		if(com_num == num)
+		if(com_num == num) {
+			com_num = 0; // 다음 요청 때 새로운 난수 생성, 저장시킴 
 			return "정답";
+		}
 		if( com_num < num)
 			return "낮춰주세요";
 		return "높여주세요";
