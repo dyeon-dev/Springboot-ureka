7ff25bf7179c1caea36227b9d39def08463f587c
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-07-19 17:54:59 +0900

    Add files via upload

---
+++ yaksok0718/src/main/java/com/ureka/myspring/Yaksok0718Application.java
@@ -0,0 +1,13 @@
+package com.ureka.myspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yaksok0718Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yaksok0718Application.class, args);
+	}
+
+}
