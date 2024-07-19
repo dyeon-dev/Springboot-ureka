8444facd9b95d447da521d2a96dfb0cb71025399
Kimdayeon <93921784+kimdayeon37@users.noreply.github.com>
2024-07-19 17:55:18 +0900

    Delete yaksok0718 directory

--- yaksok0718/src/main/java/com/ureka/myspring/YaksokRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ureka.myspring;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ureka.myspring.entity.Yaksok;
-
-public interface YaksokRepository extends JpaRepository<Yaksok, Integer> {
-	
-}
